383b14e501063636aa4b7d06c882d3d3f4a3614a
rdiaconu0 <radu0071@gmail.com>
2025-05-08 22:30:52 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,27 +4,26 @@ pub mod rocblas;
 pub mod rocfft;
 pub mod rocrand;
 pub mod rocsolver;
-
+pub use hip::*;
+pub use miopen::*;
 pub mod error;
 pub mod hip;
 use crate::rocfft::examples;
 
 #[cfg(feature = "rocm_smi")]
 pub mod rocmsmi;
-pub mod rocsparse;
 mod rocprofiler;
+pub mod rocsparse;
 
 #[cfg(test)]
 mod tests {
     use crate::hip;
-    use crate::rocprofiler::types::{Feature, InfoData, Parameter, ParameterName, ProfilerMode};
-    use crate::hip::{device_synchronize, get_device_count, Device, DeviceMemory};
+    use crate::hip::{Device, DeviceMemory, device_synchronize, get_device_count};
     use crate::rocfft::examples::run_1d_complex_example;
     use crate::rocprofiler::context::Properties;
-    use crate::rocprofiler::profiler::{get_metrics, version_string, Profiler};
+    use crate::rocprofiler::profiler::{Profiler, get_metrics, version_string};
+    use crate::rocprofiler::types::{Feature, InfoData, Parameter, ParameterName, ProfilerMode};
     use crate::rocrand::utils::{generate_normal_f32, generate_uniform_f64};
-
-
 
     #[test]
     fn test_rocprofiler_simple() {
@@ -39,10 +38,12 @@ mod tests {
 
         let device = match Device::new(0) {
             Ok(dev) => {
-                let props = dev.properties().unwrap_or_else(|_| panic!("Couldn't get device properties"));
+                let props = dev
+                    .properties()
+                    .unwrap_or_else(|_| panic!("Couldn't get device properties"));
                 println!("Using device: {}", props.name);
                 dev
-            },
+            }
             Err(e) => {
                 println!("Failed to get device: {}", e);
                 return;
@@ -50,23 +51,22 @@ mod tests {
         };
 
         // Try with a single widely-supported metric
-        let features = vec![
-            Feature::new_metric("GRBM_COUNT", vec![
-                Parameter::new(ParameterName::SeMask, 0xffffffff)
-            ]),
-        ];
+        let features = vec![Feature::new_metric(
+            "GRBM_COUNT",
+            vec![Parameter::new(ParameterName::SeMask, 0xffffffff)],
+        )];
 
         println!("Creating profiler...");
         let profiler = match Profiler::new(
             device,
             features,
             &[ProfilerMode::Standalone, ProfilerMode::SingleGroup],
-            None
+            None,
         ) {
             Ok(p) => {
                 println!("Profiler created successfully");
                 p
-            },
+            }
             Err(e) => {
                 println!("Failed to create profiler: {} (code: {})", e, e.code());
                 return;
